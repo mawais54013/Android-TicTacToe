20b83e714df00c550a2f0ad3777d2768996793e6
MAwais <mawais54013@outlook.com>
2019-08-21 18:58:21 -0700

    Finished Main Code

--- app/src/main/java/com/muhammadawais/tictactoe/MainActivity.java
+++ app/src/main/java/com/muhammadawais/tictactoe/MainActivity.java
@@ -1,5 +1,7 @@
 package com.muhammadawais.tictactoe;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -45,21 +47,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                resetGame();
             }
         });
     }
 
     @Override
-    public void onClick(View view) {
-        if (((Button) view).getText().toString().equals("")) {
+    public void onClick(View v) {
+        if (!((Button) v).getText().toString().equals("")) {
             return;
         }
 
-        if(player1Turn) {
-            ((Button) view).setText("X");
+        if (player1Turn) {
+            ((Button) v).setText("X");
         } else {
-            ((Button) view).setText("O");
+            ((Button) v).setText("O");
         }
 
         roundCount++;
@@ -70,7 +72,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             } else {
                 player2Wins();
             }
-        } else if(roundCount == 9) {
+        } else if (roundCount == 9) {
             draw();
         } else {
             player1Turn = !player1Turn;
@@ -130,5 +132,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         resetBoard();
     }
 
-    
+    private void updatePointsText() {
+        textViewPlayer1.setText("Player 1: " + player1Points);
+        textViewPlayer2.setText("Player 2: " + player2Points);
+    }
+
+    private void resetBoard() {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                buttons[i][j].setText("");
+            }
+        }
+
+        roundCount = 0;
+        player1Turn = true;
+    }
+
+    private void resetGame() {
+        player1Points = 0;
+        player2Points = 0;
+        updatePointsText();
+        resetBoard();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("roundCount", roundCount);
+        outState.putInt("player1Points", player1Points);
+        outState.putInt("player2Points", player2Points);
+        outState.putBoolean("player1Turn", player1Turn);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        roundCount = savedInstanceState.getInt("roundCount");
+        player1Points = savedInstanceState.getInt("player1Points");
+        player2Points = savedInstanceState.getInt("player2Points");
+        player1Turn = savedInstanceState.getBoolean("player1Turn");
+    }
 }
