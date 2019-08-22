b5b22f8a8e9948d9a0c1d047e79762cfc75025d0
MAwais <mawais54013@outlook.com>
2019-08-21 20:32:26 -0700

    Added Firebase

--- app/src/main/java/com/muhammadawais/tictactoe/LoginActivity.java
+++ app/src/main/java/com/muhammadawais/tictactoe/LoginActivity.java
@@ -2,17 +2,26 @@ package com.muhammadawais.tictactoe;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
-    // TODO: Add member variables here:
+    private FirebaseAuth mAuth;
     // UI references.
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
@@ -36,13 +45,13 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        // TODO: Grab an instance of FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
 
     }
 
     // Executed when Sign in button pressed
     public void signInExistingUser(View v)   {
-        // TODO: Call attemptLogin() here
+        attemptLogin();
 
     }
 
@@ -53,17 +62,34 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    // TODO: Complete the attemptLogin() method
     private void attemptLogin() {
 
 
-        // TODO: Use FirebaseAuth to sign in with email & password
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
 
+        if(email.equals("") || password.equals(""))
+        return;
 
+        Toast.makeText(this, "Login in progress", Toast.LENGTH_SHORT).show();
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(!task.isSuccessful()) {
+                    Log.d("test", "user creation failed");
+                    showErrorDialog("problem logging in");
+                } else {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    finish();
+                    startActivity(intent);
+                }
+            }
+        });
 
     }
 
-    // TODO: Show error on screen with an alert dialog
-
-
+    private void showErrorDialog(String message) {
+        new AlertDialog.Builder(this).setTitle("Oops").setMessage(message).setPositiveButton(android.R.string.ok, null).show();
+    }
 }
